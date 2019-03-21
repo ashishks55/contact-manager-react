1fb15b83c6042ff203d385af9584a7c69892270a
Ashish Kanwar Singh <ashish@Ashishs-MacBook-Pro.local>
2019-03-21 19:47:06 +0530

    Updates

---
+++ precache-manifest.cf709e040222d151ab2c3c81f5ac0c9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62f6f0a18501245db0cd",
+    "url": "/ashishks55/contact-manager-react/static/js/runtime~main.e66f2b80.js"
+  },
+  {
+    "revision": "813d21354456bf067832",
+    "url": "/ashishks55/contact-manager-react/static/js/main.93ab75cd.chunk.js"
+  },
+  {
+    "revision": "652371f65be87c6c3aac",
+    "url": "/ashishks55/contact-manager-react/static/js/2.0a11c6cd.chunk.js"
+  },
+  {
+    "revision": "813d21354456bf067832",
+    "url": "/ashishks55/contact-manager-react/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "652371f65be87c6c3aac",
+    "url": "/ashishks55/contact-manager-react/static/css/2.93753f60.chunk.css"
+  },
+  {
+    "revision": "15bc190b50d8c4ad77b25815697759c8",
+    "url": "/ashishks55/contact-manager-react/index.html"
+  }
+];
